39b24d21890f525d167a4e2f8eb2d553389f1234
guitar486 <guitar486@users.noreply.github.com>
2015-09-24 17:16:14 -0700

    + pip

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'pytvmaze',
+    version = '1.0.2',
+    description = 'Python interface to the TV Maze API (www.tvmaze.com)',
+    url = 'https://github.com/guitar486/pytvmaze',
+    author = 'Spencer Roberts',
+    license='MIT',
+
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7'
+    ],
+
+    keywords = 'python tv television tvmaze',
+    packages=['pytvmaze']
+
+)
