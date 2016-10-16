1ded6a3f75629695f477f69881d125c2b9221447
srob650 <spencer.j.roberts@gmail.com>
2016-10-16 13:53:50 -0700

    increment version to 2.0.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'pytvmaze',
-    version = '2.0.3',
+    version = '2.0.4',
     description = 'Python interface to the TV Maze API (www.tvmaze.com)',
     url = 'https://github.com/srob650/pytvmaze',
     author = 'Spencer Roberts',
